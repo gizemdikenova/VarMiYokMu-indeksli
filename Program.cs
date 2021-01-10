33fa04a52ca4d4c6de11bf19433149527343bc81
Gizem Dikenova <dikenovagizem@gmail.com>
2021-01-10 19:36:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarMiYokMu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int indexNo;
+            string[] sehirler = { "Eskişehir", "İstanbul", "İzmir", "Diyarbakır", "Sivas", "Edirne", "Bursa", "Afyon", "Malatya", "Tekirdag", "Ankara" };
+            Console.WriteLine("Bir şehir giriniz");
+            string girilensehir = Console.ReadLine();
+
+            bool varMi = false;
+            for (int i = 0; i < sehirler.Length; i++)
+            {
+                if (sehirler[i] == girilensehir)
+                {
+                    Console.WriteLine($"Aranılan şehir {girilensehir} ve indeksi= {i}");
+                    varMi = true;
+                    break;
+                }
+            }
+
+            if (!varMi)
+            {
+                Console.WriteLine("Listede böyle bir şehir bulunmamaktadır.");
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
